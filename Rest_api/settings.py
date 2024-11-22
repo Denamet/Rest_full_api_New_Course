673f2a6fe1ec17fb239c2b0592285b7a7d0bf8a2
ahmed saeyd <asame8855@gmail.com>
2024-11-22 03:47:24 +0200

    the new commit by ahmed saeed

--- Rest_api/settings.py
+++ Rest_api/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'profiles_api.User_profiels'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin , BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+
+    
+    
+    def create_user(self , email , name , password=None):
+        if not email :
+            raise ValueError ("User must have an email address ! ")
+        
+
+        email = self.normalize_email(email)
+        user = self.model(email=email , name =name)
+        user.set_password(password)
+        user.save(using = self._db)
+        
+        return user
+ 
+    def create_superuser(self , email , name , password):
+        user = self.create_user(email , name , password)
+        user.is_supperuser = True 
+        user.is_staff = True 
+        user.save(using=self._db)
+        return user 
+
+
+class User_profiels(AbstractBaseUser , PermissionsMixin) :
+    email = models.EmailField(max_length=255 , unique=True)
+    name = models.CharField(max_length=100 )
+    is_acttive = models.BooleanField(default=True)
+    is_sttaf = models.BooleanField(default=False)
+
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD =  'email'
+    REQUIRED_FIELDS = [' name']
+
+
+    def get_full_name(self):
+        return self.email 
+
+    def get_smal_name(self):
+        return self.name 
+
+
+    def __str__(self) :
+        return self.email
+
+
